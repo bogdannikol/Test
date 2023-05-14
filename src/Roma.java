c6f2f99de9772824263610acc049edb24a14dca3
bogdannikol <slon24306@gmail.com>
2023-05-14 07:26:05 +0300

    2023.05.14.

---
+++ src/Roma.java
@@ -0,0 +1,44 @@
+public enum Roma {
+     i(1), ii(2), iii(3), iv(4), v(5), vi(6),
+    vii(7), viii(8), ix(9), x(10), xi(11), xii(12), xiii(13),
+    xiv(14), xv(15), xvi(16), xvii(17), xviii(18), xix(19),
+    xx(20), xxi(21), xxii(22), xxiii(23), xxiv(24), xxv(25),
+    xxvi(26), xxvii(27), xxviii(28), xxix(29), xxx(30), xxxi(31),
+    xxxii(32), xxxiii(33), xxxiv(34), xxxv(35), xxxvi(36), xxxvii(37),
+    xxxviii(38), xxxix(39), xl(40), xli(41), xlii(42), xliii(43),
+    xliv(44), xlv(45), xlvi(46), xlvii(47), xlviii(48), xlix(49),
+    l(50), li(51), lii(52), liii(53),liv(54), lv(55), lvi(56),
+    lvii(57), lviii(58), lix(59), lx(60), lxi(61), lxii(62),
+    lxiii(63), lxiv(64), lxv(65), lxvi(66), lxvii(67), lxviii(68),
+    lxix(69), lxx(70), lxxi(71), lxxii(72), lxxiii(73), lxxiv(74),
+    lxxv(75), lxxvi(76), lxxvii(77), lxxviii(78), lxxix(79), lxxx(80),
+    lxxxi(81), lxxxii(82), lxxxiii(83), lxxxiv(84), lxxxv(85),
+    lxxxvi(86), lxxxvii(87), lxxxviii(88), lxxxix(89), xc(90), xci(91),
+    xcii(92), xciii(93), xciv(94), xcv(95), xcvi(96), xcvii(97),
+    xcviii(98), xcix(99), c(100),
+    I(1), II(2), III(3), IV(4), V(5), VI(6),
+    VII(7), VIII(8), IX(9), X(10), XI(11), XII(12), XIII(13),
+    XIV(14), XV(15), XVI(16), XVII(17), XVIII(18), XIX(19),
+    XX(20), XXI(21), XXII(22), XXIII(23), XXIV(24), XXV(25),
+    XXVI(26), XXVII(27), XXVIII(28), XXIX(29), XXX(30), XXXI(31),
+    XXXII(32), XXXIII(33), XXXIV(34), XXXV(35), XXXVI(36), XXXVII(37),
+    XXXVIII(38), XXXIX(39), XL(40), XLI(41), XLII(42), XLIII(43),
+    XLIV(44), XLV(45), XLVI(46), XLVII(47), XLVIII(48), XLIX(49),
+    L(50), LI(51), LII(52), LIII(53),LIV(54), LV(55), LVI(56),
+    LVII(57), LVIII(58), LIX(59), LX(60), LXI(61), LXII(62),
+    LXIII(63), LXIV(64), LXV(65), LXVI(66), LXVII(67), LXVIII(68),
+    LXIX(69), LXX(70), LXXI(71), LXXII(72), LXXIII(73), LXXIV(74),
+    LXXV(75), LXXVI(76), LXXVII(77), LXXVIII(78), LXXIX(79), LXXX(80),
+    LXXXI(81), LXXXII(82), LXXXIII(83), LXXXIV(84), LXXXV(85),
+    LXXXVI(86), LXXXVII(87), LXXXVIII(88), LXXXIX(89), XC(90), XCI(91),
+    XCII(92), XCIII(93), XCIV(94), XCV(95), XCVI(96), XCVII(97),
+    XCVIII(98), XCIX(99), C(100);
+
+  private int translation;
+    Roma(int translation){
+        this.translation = translation;
+    }
+    public int getTranslation() {
+        return translation;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) throws RuntimeException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите числа римские или арабские от 1 до 10 в формате:\nчисло1 пробел оперант пробел число2: ");
+        String str1 = scanner.nextLine();
+        String[] strings = str1.split(" ");
+        int size = strings.length;
+        if (size==1){
+            throw new ArithmeticException("Строка не является математической операцией!");
+        }
+        if (size>3){
+            throw new ArithmeticException("В операции должно быть два операнда и один оператор (+, -, /, *)");
+        }
+        String arab1 = strings[0];
+        String arab2 = strings[2];
+        String proverka = (strings[2]+strings[0]);
+
+        if (proverka.matches("(([a-zA-Z].*[0-9])|([0-9].*[a-zA-Z]))")){
+            throw new ArithmeticException("Используются одновременно разные системы счисления");
+        }
+        if (proverka.matches("^[0-9]*$")) {
+            int ar1=Integer.parseInt(arab1);
+            int ar2=Integer.parseInt(arab2);
+            if (ar1>10 ||ar2>10) {
+                throw new ArithmeticException("Введённое число больше допустимого!!!\nДиапазон чисел от 1 до 10");
+            }
+
+           if (strings[1].hashCode() == 42) System.out.println(ar1 * ar2);
+           if (strings[1].hashCode() == 47) System.out.println(ar1 / ar2);
+           if (strings[1].hashCode() == 43) System.out.println(ar1 + ar2);
+           if (strings[1].hashCode() == 45) System.out.println(ar1 - ar2);
+           else System.exit(0);
+            }
+        String a = strings[0];
+        String b = strings[2];
+            Roma perevod1 = Roma.valueOf(a);
+            Roma perevod2 = Roma.valueOf(b);
+            int num1=perevod1.getTranslation();
+            int num2=perevod2.getTranslation();
+            int hesh=strings[1].hashCode();
+            int otvet;
+          if  (num1<=10 && num2<=10 && hesh == 42) {
+              otvet = num1 * num2;
+              Roma[] romas = Roma.values();
+              Roma found = null;
+              for (Roma roma : romas) {
+                  if (roma.getTranslation() == otvet) {
+                      found = roma;
+                  }
+              }
+              System.out.println(found);
+              System.exit(0);
+          }
+          else if (num1 <= 10 && num2<= 10 && hesh == 43) {
+                  otvet = num1 + num2;
+              Roma[] romas = Roma.values();
+              Roma found = null;
+              for (Roma roma : romas) {
+                  if (roma.getTranslation() == otvet) {
+                      found = roma;
+                  }
+              }
+              System.out.println(found);
+              System.exit(0);
+          }
+          else if (num1<=10 && num2<=10 && hesh==45) {
+               if (num1 < num2){
+                   throw new ArithmeticException("В римскиих числах нет отрицательного знака!");
+               }
+                      otvet = num1 - num2;
+              if (otvet==0){
+                  throw new ArithmeticException("В римских цифрах нет нулевого значения!");
+              }
+              Roma[] romas = Roma.values();
+              Roma found = null;
+              for (Roma roma : romas) {
+                  if (roma.getTranslation() == otvet) {
+                      found = roma;
+                  }
+              }
+              System.out.println(found);
+              System.exit(0);
+          }
+          else if (num1 <= 10 && num2 <= 10 && hesh == 47) {
+              otvet = num1 / num2;
+              Roma[] romas = Roma.values();
+              Roma found = null;
+              for (Roma roma : romas) {
+                  if (roma.getTranslation() == otvet) {
+                      found = roma;
+                  }
+              }
+              if (otvet<1){
+                  throw new ArithmeticException("В римских цифрах нет нулевого значения!");
+              }
+              System.out.println(found);
+              System.exit(0);
+          }
+          else if ((num1 > 10 | num1 > 10)) {
+              throw new ArithmeticException("Введённое число больше допустимого!!!\nДиапазон чисел от 1 до 10");
+          }
+    }
+}
